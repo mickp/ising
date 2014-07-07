16c2872e381a1aa86253769b2c5f4c8ce8c57437
map <mick.phillips@gmail.com>
2014-07-07 20:11:00 +0100

    Replaced exp with a hash lookup.

--- ising.py
+++ ising.py
@@ -53,6 +53,15 @@ class Ising2d(object):
         n = self.percyc or (N * N / 4)
         p = np.random.random_integers(0, N-1, n)
         q = np.random.random_integers(0, N-1, n)
+
+        ## Evaluating np.exp(- (newH - H) / (kB * self.T)).
+        # Use a hash, instead.
+        p_flip = {-8: np.exp(8 / (kB * self.T)),
+                  -4: np.exp(4 / (kB * self.T)),
+                   0: 1.0,
+                   4: np.exp(-4 / (kB * self.T)),
+                   8: np.exp(-8 / (kB * self.T))}
+
         for i in range(n):
             index = (p[i], q[i])
             H = mu * lattice[index] \
@@ -65,7 +74,8 @@ class Ising2d(object):
                              for delta, J in zip(neighbours, coeffs)])
             if newH <= H:
                 lattice[index] *= -1
-            elif np.exp(- (newH - H) / (kB * self.T)) >= np.random.rand():
+            #elif np.exp(- (newH - H) / (kB * self.T)) >= np.random.rand():
+            elif p_flip[int(round(newH - H))] > np.random.rand():
                 lattice[index] *= -1
 
         self.histT.append(self.T)
